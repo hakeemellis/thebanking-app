243f7963ff7eec167faea0f89fd15591d03e0a6d
hakeemellis <114674803+hakeemellis@users.noreply.github.com>
2024-05-20 22:00:47 -0400

    Installed TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  darkMode: 'class', // to allow "dark mode" to be applied when a class is assigned to html
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
